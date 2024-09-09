7e4a4bb98d254873bfba8616a1488ca2407d8793
cimadonaline <54597526+cimadonaline@users.noreply.github.com>
2024-09-09 16:48:02 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
-    // Obtém o valor do campo de pesquisa, converte para minúsculas para pesquisa case-insensitive
     let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-    if (!campoPesquisa.trim() || /^[.,\s]+$/.test(campoPesquisa)) {
-        // Se a pesquisa for inválida, exibe uma mensagem e encerra a função
-        section.innerHTML = "<p>Nenhum resultado encontrado. Por favor, insira um termo de pesquisa válido.</p>";
-        return;
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+        return 
     }
 
-    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let tags = "";
 
-    // Itera sobre cada item de dados (assumindo que 'dados' é um array de objetos)
+    // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        let titulo = dado.titulo.toLowerCase();
-        let descricao = dado.descricao.toLowerCase();
-        let tags = dado.tag.toLowerCase();
-
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
         if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-            // Se a palavra foi encontrada, cria um elemento HTML para o resultado
+            // cria um novo elemento
             resultados += `
-                <div class="item-resultado">  
-                    <h2>
-                        <a href="${dado.link}" target="_blank">
-                            ${dado.titulo.replace(new RegExp(campoPesquisa, 'gi'), match => `<mark>${match}</mark>`)}
-                        </a>
-                    </h2>
-                    <p class="descricao-meta">${dado.descricao}</p>
-                    <a href="${dado.link}">Mais informações</a>
-                </div>
-            `;
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
         }
     }
 
-    // Se nenhum resultado foi encontrado, exibe uma mensagem
     if (!resultados) {
-        resultados = "<p>Nenhum resultado encontrado.</p>";
+        resultados = "<p>Nada foi encontrado</p>"
     }
 
-    // Atualiza o conteúdo HTML da seção com os resultados da pesquisa
+    // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
 }
